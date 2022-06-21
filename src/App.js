80aaf63c22ecd8a7f9c86be2d5187c2ce0ca2d15
Bahromjon050 <bahromjonsobitxanov468@gmail.com>
2022-06-21 11:39:11 +0500

    Users

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
 import AOS from 'aos'
-import { Account, Navbar } from './components/All';
-import ResponsiveDrawer from './components/Mui';
-import { Batafsil } from './components/Youtube';
 import { Github } from './components/Malumot';
 AOS.init()
 function App() {
   return (
     <>
       <div className="padding">
-        {/* <Navbar /> */}
-        {/* <Batafsil /> */}
         <Github />
       </div>
     </>
--- src/components/Malumot.js
+++ src/components/Malumot.js
@@ -5,22 +5,22 @@ export const Github = () => {
     const [input, setInput] = useState('')
     const [massiv, setMassiv] = useState([])
     const sendFun = () => {
-        console.log(massiv);
-        axios.get(`https://api.github.com/user/${input}`)
-        .then((res) => {
-            console.log(res.data);
-            setMassiv([...massiv, res.data])
-        })
+        // console.log(massiv);
+        axios.get(`https://api.github.com/users/${input}`)
+            .then((res) => {
+                // console.log(res.data);
+                setMassiv([...massiv, res.data])
+            })
     }
     useEffect(() => {
-        axios.get(`https://api.github.com/user/${input}`)
-        .then((res) => {
-            console.log(res.data);
-            setMassiv([...massiv, res.data])
-        })
+        axios.get(`https://api.github.com/users/${input}`)
+            .then((res) => {
+                // console.log(res.data);
+                setMassiv([...massiv, res.data])
+            })
     }, []);
     const searchFun = (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         setInput(e.target.value)
     }
     const reset = () => {
@@ -28,26 +28,28 @@ export const Github = () => {
         setInput('')
     }
     return (
-    <>
-        <div className="github">
-            <input type="text" className='inputSearch' value={input} onChange={searchFun} />
-            <button onClick={sendFun}>Send</button>
-            <button className='resetBtn' onClick={reset}>{input === "" ? null : "X"}</button>
-            <div className="cards">
-                {
-                    massiv.map((val) => (
-                        <div className="card">
-                            <img src={val.avatar_url} alt="" />
-                            <h2>Name : {val.name}</h2>
-                            <h2>username : {val.username}</h2>
-                            <h2>Id : {val.id}</h2>
-                            <h2>country : {val.city === "" ? "topilmadi" : val.city}</h2>
-                            <h2>email : {val.email}</h2>
-                        </div>
-                    ))
-                }
+        <>
+            <div className="github">
+                <input type="text" className='inputSearch' value={input} onChange={searchFun} />
+                <button onClick={sendFun}>Send</button>
+                <button className='resetBtn' onClick={reset}>{input === "" ? null : "X"}</button>
+                <div className="cards">
+                    {
+                        massiv.map((val) => (
+                            <div className="card">
+                                <img src={val.avatar_url} alt="" />
+                                <h2>Name : {val.name}</h2>
+                                <h2>Username : {val.login}</h2>
+                                <h2>Bio : {val.bio}</h2>
+                                <h2>Company : {val.company}</h2>
+                                <h2>Repositories : {val.public_repos}</h2>
+                                <h2>Followers : {val.followers}</h2>
+                                <h2>Following : {val.following}</h2>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
